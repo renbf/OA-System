91df8e771069d247e298c7853f48f621cd73ff4e
apollour <apollou@163.com>
2020-09-25 15:33:41 +0800

    '前端数据字典放入缓存'

--- OASsystem-ui/src/views/index.vue
+++ OASsystem-ui/src/views/index.vue
@@ -25,7 +25,7 @@
       </el-col>
     </el-row>
 
-    
+
   </div>
 </template>
 
@@ -73,7 +73,11 @@ export default {
     handleSetLineChartData(type) {
       this.lineChartData = lineChartData[type]
     }
+  },
+  created() {
+    this.common.getAllDict();
   }
+
 }
 </script>
 
