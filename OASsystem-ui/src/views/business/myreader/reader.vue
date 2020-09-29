72d173e4c5ad6d5e8accd713d55ef602ba36a7a4
SUNMENGYAO <2390215700@qq.com>
2020-09-29 14:16:36 +0800

    待办

--- OASsystem-ui/src/views/business/myreader/reader.vue
+++ OASsystem-ui/src/views/business/myreader/reader.vue
@@ -13,12 +13,75 @@
     <el-card class="box-card" >
       <div slot="header" class="clearfix">
 
-        <span class="big" >加班审批</span>
+        <span class="big" >请假审批</span>
         <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
       </div>
       <div class="father"> <span class="son">待审批</span></div>
-      <div class="father"> <span class="conten">12</span>件</div>
+      <div class="father"> <span class="conten">0</span>件</div>
     </el-card>
+    <el-card class="box-card" >
+      <div slot="header" class="clearfix">
+
+        <span class="big" >面试申请</span>
+        <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+      </div>
+      <div class="father"> <span class="son">待审批</span></div>
+      <div class="father"> <span class="conten">0</span>件</div>
+    </el-card>
+    <el-card class="box-card" >
+      <div slot="header" class="clearfix">
+
+        <span class="big" >差旅费报销（元）</span>
+        <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+      </div>
+      <div class="father"> <span class="son">待审批</span></div>
+      <div class="father"> <span class="conten">0</span>件</div>
+    </el-card>
+    <el-card class="box-card" >
+      <div slot="header" class="clearfix">
+
+        <span class="big" >费用报销</span>
+        <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+      </div>
+      <div class="father"> <span class="son">待审批</span></div>
+      <div class="father"> <span class="conten">0</span>件</div>
+    </el-card>
+    <el-card class="box-card" >
+      <div slot="header" class="clearfix">
+
+        <span class="big" >入职申请</span>
+        <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+      </div>
+      <div class="father"> <span class="son">待审批</span></div>
+      <div class="father"> <span class="conten">0</span>件</div>
+    </el-card>  <el-card class="box-card" >
+    <div slot="header" class="clearfix">
+
+      <span class="big" >离职申请</span>
+      <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+    </div>
+    <div class="father"> <span class="son">待审批</span></div>
+    <div class="father"> <span class="conten">0</span>件</div>
+  </el-card>  <el-card class="box-card" >
+    <div slot="header" class="clearfix">
+
+      <span class="big" >调岗申请</span>
+      <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+    </div>
+    <div class="father"> <span class="son">待审批</span></div>
+    <div class="father"> <span class="conten">0</span>件</div>
+  </el-card>  <el-card class="box-card" >
+    <div slot="header" class="clearfix">
+
+      <span class="big" >xxx项目组申请</span>
+      <el-button class="little" style="float: right" icon="el-icon-right" circle size="mini"></el-button>
+    </div>
+    <div class="father"> <span class="son">待审批</span></div>
+    <div class="father"> <span class="conten">0</span>件</div>
+  </el-card>
+
+
+
   </div>
 
 
@@ -41,12 +104,15 @@
   }
 .little{
   margin-top: -2px;
+
 }
+
 
   .box-card {
     width: 316px;
     height: 184px;
-    margin: 20px;
+    margin-left: 20px;
+    margin-top: 18px;
   }
   .father{
     text-align: center;
