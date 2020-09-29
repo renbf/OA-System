672690e0b316372fa001e1a7fb7b72b98fcb9d44
SUNMENGYAO <2390215700@qq.com>
2020-09-29 17:30:17 +0800

    分页

--- OASsystem-ui/src/views/business/myreader/little.vue
+++ OASsystem-ui/src/views/business/myreader/little.vue
@@ -102,6 +102,15 @@
     </el-table-column>
 
     </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
 
 
   </div>
@@ -242,6 +251,12 @@
       goBack(){
 
         this.$router.push({ path:'/myreader/reader'})
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
       }
     }
   }
@@ -263,5 +278,8 @@
   margin-left: 20px;
   margin-top: 30px;
 }
+  .el-pagination{
+    float:right;
+  }
 
 </style>
