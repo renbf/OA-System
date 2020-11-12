c46e31ee30ca4e1bb5593d91909795f4893e1938
apollour <apollou@163.com>
2020-11-12 10:20:44 +0800

    GIS

--- OASsystem-ui/src/views/business/arcgis/index.vue
+++ OASsystem-ui/src/views/business/arcgis/index.vue
@@ -58,14 +58,4 @@
     }
   };
 </script>
-<style scoped>
-  body {
-    margin: 0;   /**主要是去除谷歌浏览器默认的8像素的外边距 */
-  }
-  #app {
-    position: absolute;   /**使这个div的大小撑满整个屏幕 */
-    width: 100%;
-    height: 100%;
-  }
-</style>
 
